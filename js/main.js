95648d09887e0aaaf0731ac759fdb55d1d37f84b
Ryan Long <ryan@carpc.(none)>
2010-03-05 17:55:17 -0600

    Changed the flashFix() function to actually work... Changed the theme credit box to reference a script so I can change it without having Marshall change the template

--- js/main.js
+++ js/main.js
@@ -9,89 +9,117 @@ $.expr[':'].highres = function(obj){
 };
 
 function fixFlash(){
-  // Most Browsers
-  $("embed").attr("wmode", "transparent");
-  // IE
-  var embedTag;
-  $("embed").each(function(i) {
-      embedTag = this.outerHTML;
-      if ((embedTag != null) && (embedTag.length > 0)) {
-          embedTag = embedTag.replace(/embed /gi, "embed wmode=\"transparent\" ");
-          this.outerHTML = embedTag;
+  // For embed
+  $("embed", this).each(function (i) {
+    var elClone = this.cloneNode(true);
+    elClone.setAttribute("wmode", "transparent");
+    $(this).before(elClone);
+    $(this).remove();
+  });
+  // For object and/or embed into objects
+  $("object", this).each(function (i, v) {
+    var elEmbed = $(this).children("embed");
+    if (typeof(elEmbed.get(0)) != "undefined") {
+      if (typeof(elEmbed.get(0).outerHTML) != "undefined") {
+        elEmbed.attr("wmode", "transparent");
+        $(this.outerHTML).insertAfter(this);
+        $(this).remove();
       }
-      else {
-          $(this).wrap("<div></div>");
+      return true;
+    }
+    var algo = this.attributes;
+    var str_tag = '<object ';
+    for (var i = 0; i < algo.length; i++) str_tag += algo[i].name + '="' + algo[i].value + '" ';
+    str_tag += '>';
+    var flag = false;
+    $(this).children().each(function (elem) {
+      if (this.nodeName == "param") {
+        if (this.name == "wmode") {
+          flag = true;
+          str_tag += '<param name="' + this.name + '" value="transparent">';
+        }
+        else str_tag += '<param name="' + this.name + '" value="' + this.value + '">';
       }
+    });
+    if (!flag) str_tag += '<param name="wmode" value="transparent">';
+    str_tag += '</object>';
+    $(str_tag).insertAfter(this);
+    $(this).remove();
   });
 }
 
 function alterContentBlock(){
-  // Add 'external' CSS class to all external links
-  $('a:external').each(function(){
-    $(this).addClass('external');
-    //$(this).attr('target', '_blank');
-  });
+  // Look at all the new (unprocessed) posts and process them
+  $('.post:not(.processed)').each(function(){
 
-  // Enable DHTML features on photo posts that have not yet been processed
-  $('.photo:not(.processed)').each(function(){
-    $this = $(this);
-    $this.data('hover-info', $('.hover-info'));
-    // Enable Colorbox
-    $("a:highres", this).colorbox({
-      maxWidth: '98%',
-      maxHeight: '98%',
-      photo: true,
-      transition: 'elastic', 
-      opacity: 0.8,
-      close: 'click anywhere to return', 
-      title: function(){
-        return $(this).parent('.photo').find('img').attr('alt');
-      },
-      rel: 'nofollow'
-    });
-    // Assign the .hover-info the button class, making it appear on top of the image. Then hide it...
-    $('.hover-info',this).addClass('button').hide();
-    
-    // ...except for on hover of the image
-    $this.add('.hover-info',this).mouseenter(function(){
-        // look for a child .hover-info. In the case of being called from .hover-info, nothing will happen :-)
-        $('.hover-info', this).show();
-      }).mouseleave(function(){
-        $('.hover-info', this).hide();
-      }
-    );
-    
-    // Add note informing user of a 'click-through link' on the photo, i.e. that clicking won't have 
-    // the same effect as on most photos.
+    // Add 'external' CSS class to all external links
     $('a:external', this).each(function(){
-      $(this).siblings('.hover-info').prepend('This photo links to: '+this.hostname+'. ');
+      $(this).addClass('external');
+      //$(this).attr('target', '_blank');
     });
-    
-    // On image load, set the `right` property of the .hover-info to sit at the right side of the photo
-    $('img',this).load(function(){
-      var newRight = (500 - $(this).width()) / 2;
-      $(this).parents('.photo').find('.hover-info').css({right: newRight });
-    });
-    
-    // Set this photo as processed so we don't do it on next-page AJAX load
-    $(this).addClass('processed');
-  });  
-  
-  // Look at all audio posts, link the artist, track name, and album name to a last.fm search for that name
-  $('.audio-post .info dl dd').css({'text-decoration': 'underline', 'cursor': 'pointer'}).click(function(){
-    $.fn.colorbox({
-      href: 'http://www.last.fm/search?q='+escape($(this).text()),
-      width: '75%',
-      height: '75%',
-      iframe: true,
-      opacity: 0.5,
-      close: 'close the popup'
+
+    // Enable DHTML features on photos
+    $('.photo', this).each(function(){
+      $this = $(this);
+      //$this.data('hover-info', $('.hover-info'));
+      // Enable Colorbox
+      $("a:highres", this).colorbox({
+        maxWidth: '98%',
+        maxHeight: '98%',
+        photo: true,
+        transition: 'elastic', 
+        opacity: 0.8,
+        close: 'click anywhere to return', 
+        title: function(){
+          return $(this).parent('.photo').find('img').attr('alt');
+        },
+        rel: 'nofollow'
+      });
+      // Assign the .hover-info the button class, making it appear on top of the image. Then hide it...
+      $('.hover-info',this).addClass('button').hide();
       
+      // ...except for on hover of the image
+      $this.add('.hover-info',this).mouseenter(function(){
+          // look for a child .hover-info. In the case of being called from .hover-info, nothing will happen :-)
+          $('.hover-info', this).show();
+        }).mouseleave(function(){
+          $('.hover-info', this).hide();
+        }
+      );
+      
+      // Add note informing user of a 'click-through link' on the photo, i.e. that clicking won't have 
+      // the same effect as on most photos.
+      $('a:external', this).each(function(){
+        $(this).siblings('.hover-info').prepend('This photo links to: '+this.hostname+'. ');
+      });
+      
+      // On image load, set the `right` property of the .hover-info to sit at the right side of the photo
+      $('img',this).load(function(){
+        var newRight = (500 - $(this).width()) / 2;
+        $(this).parents('.photo').find('.hover-info').css({right: newRight });
+      });
+
+    });  
+    
+    // Look at all audio posts, link the artist, track name, and album name to a last.fm search for that name
+    $('.audio-post .info dl dd', this).css({'text-decoration': 'underline', 'cursor': 'pointer'}).click(function(){
+      $.fn.colorbox({
+        href: 'http://www.last.fm/search?q='+escape($(this).text()),
+        width: '75%',
+        height: '75%',
+        iframe: true,
+        opacity: 0.5,
+        close: 'close the popup'
+        
+      });
     });
+    
+    // Fix all flash on the page to have the wmode param set to transparent
+    fixFlash();
+    
+    // Set this post as processed so we don't do it on next-page AJAX load
+    $(this).addClass('processed');
   });
-  
-  // Fix all flash on the page to have the wmode param set to transparent
-  fixFlash();
 }
 
 $(document).ready(function(){
