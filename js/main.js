51724cdaac89d475468920712659b36ee6bedc9e
Ryan Taylor Long <ryan@rtlong.com>
2010-03-08 15:50:42 -0600

    Added the DISQUS Comments System, a return-to-top button, Moved all script inclusions to the bottom of the page
    - Added post notes 'summary' which includes a link to show the sotes, which are now hidden, by default
    - Added the return-to-top button (or completed it)
    - Fixed Hamburglr's meta_var method to perform checking (whether an accessed variable was defined)
    - Fixed a few bugs (mistakes, really) in main.js, incuding moving fixFlash() into the alterContentBlock() function.
    - Installed DISQUS to the theme
    - Moved jQuery to assets.rtlong.com
    - Added PagesListHeaderText to the top of the list of 'Pages'
    - Moved all script inclusions to the bottom of the page to facilitate the appearance of a faster page.

--- js/main.js
+++ js/main.js
@@ -8,56 +8,15 @@ $.expr[':'].highres = function(obj){
     return obj.href.match(/^http\:\/\/[\w.]+\/photo\/\d+\/\d+/) && (obj.hostname == location.hostname);
 };
 
-function fixFlash(){
-  // For embed
-  $("embed", this).each(function (i) {
-    var elClone = this.cloneNode(true);
-    elClone.setAttribute("wmode", "transparent");
-    $(this).before(elClone);
-    $(this).remove();
+window.alterContentBlock = function(){
+  // Add 'external' CSS class to all external links
+  $('a:external').each(function(){
+    $(this).addClass('external');
+    //$(this).attr('target', '_blank');
   });
-  // For object and/or embed into objects
-  $("object", this).each(function (i, v) {
-    var elEmbed = $(this).children("embed");
-    if (typeof(elEmbed.get(0)) != "undefined") {
-      if (typeof(elEmbed.get(0).outerHTML) != "undefined") {
-        elEmbed.attr("wmode", "transparent");
-        $(this.outerHTML).insertAfter(this);
-        $(this).remove();
-      }
-      return true;
-    }
-    var algo = this.attributes;
-    var str_tag = '<object ';
-    for (var i = 0; i < algo.length; i++) str_tag += algo[i].name + '="' + algo[i].value + '" ';
-    str_tag += '>';
-    var flag = false;
-    $(this).children().each(function (elem) {
-      if (this.nodeName == "param") {
-        if (this.name == "wmode") {
-          flag = true;
-          str_tag += '<param name="' + this.name + '" value="transparent">';
-        }
-        else str_tag += '<param name="' + this.name + '" value="' + this.value + '">';
-      }
-    });
-    if (!flag) str_tag += '<param name="wmode" value="transparent">';
-    str_tag += '</object>';
-    $(str_tag).insertAfter(this);
-    $(this).remove();
-  });
-}
-
-function alterContentBlock(){
+  
   // Look at all the new (unprocessed) posts and process them
   $('.post:not(.processed)').each(function(){
-
-    // Add 'external' CSS class to all external links
-    $('a:external', this).each(function(){
-      $(this).addClass('external');
-      //$(this).attr('target', '_blank');
-    });
-
     // Enable DHTML features on photos
     $('.photo', this).each(function(){
       $this = $(this);
@@ -77,16 +36,18 @@ function alterContentBlock(){
       });
       // Assign the .hover-info the button class, making it appear on top of the image. Then hide it...
       $('.hover-info',this).addClass('button').hide();
-      
-      // ...except for on hover of the image
-      $this.add('.hover-info',this).mouseenter(function(){
-          // look for a child .hover-info. In the case of being called from .hover-info, nothing will happen :-)
-          $('.hover-info', this).show();
-        }).mouseleave(function(){
-          $('.hover-info', this).hide();
-        }
-      );
-      
+
+      // ...except for on hover of the image has a link or highres      
+      if($this.is(':has(a)')){
+        $this.add('.hover-info',this).mouseenter(function(){
+            // look for a child .hover-info. In the case of being called from .hover-info, nothing will happen :-)
+            $('.hover-info', this).show();
+          }).mouseleave(function(){
+            $('.hover-info', this).hide();
+          }
+        );
+      }
+            
       // Add note informing user of a 'click-through link' on the photo, i.e. that clicking won't have 
       // the same effect as on most photos.
       $('a:external', this).each(function(){
@@ -115,16 +76,67 @@ function alterContentBlock(){
     });
     
     // Fix all flash on the page to have the wmode param set to transparent
-    fixFlash();
+    $("embed", this).each(function (i) {
+      var elClone = this.cloneNode(true);
+      elClone.setAttribute("wmode", "transparent");
+      $(this).before(elClone);
+      $(this).remove();
+    });
+    // For object and/or embed into objects
+    $("object", this).each(function (i, v) {
+      var elEmbed = $(this).children("embed");
+      if (typeof(elEmbed.get(0)) != "undefined") {
+        if (typeof(elEmbed.get(0).outerHTML) != "undefined") {
+          elEmbed.attr("wmode", "transparent");
+          $(this.outerHTML).insertAfter(this);
+          $(this).remove();
+        }
+        return true;
+      }
+      var algo = this.attributes;
+      var str_tag = '<object ';
+      for (var i = 0; i < algo.length; i++){ str_tag += algo[i].name + '="' + algo[i].value + '" '; }
+      str_tag += '>';
+      var flag = false;
+      $(this).children().each(function (elem) {
+        if (this.nodeName == "param") {
+          if (this.name == "wmode") {
+            flag = true;
+            str_tag += '<param name="' + this.name + '" value="transparent">';
+          }
+          else str_tag += '<param name="' + this.name + '" value="' + this.value + '">';
+        }
+      });
+      if (!flag) str_tag += '<param name="wmode" value="transparent">';
+      str_tag += '</object>';
+      $(str_tag).insertAfter(this);
+      $(this).remove();
+    });
+    
+    // Hide the notes 
+    $("div.notes ol.notes").hide();
+    // Set the show-notes to show the notes
+    $("div.notes .show-notes").click(function(){ 
+      $("div.notes ol.notes").slideDown(); 
+      $(this).remove();
+    });
     
     // Set this post as processed so we don't do it on next-page AJAX load
     $(this).addClass('processed');
   });
+  // Get comment count for all posts
+  var disqus_query = new Array();
+  $('a[href$=#disqus_thread]').each(function(){
+    disqus_query.push('url' + (disqus_query.length+1) + '=' + encodeURIComponent( $(this).attr('href').split('#')[0] ) );
+  });
+
+  $.getScript("http://disqus.com/forums/" + window.disqus_shortname + "/get_num_replies.js?" + disqus_query.join('&') );
 }
 
 $(document).ready(function(){
+  //console.log('Document ready!');
   // set callbacks, etc.
-  alterContentBlock();
+  window.alterContentBlock();
   
   // shift the Tumblr controls over a bit so they don't cover the sidebar
   $('#tumblr_controls').css({
@@ -132,6 +144,7 @@ $(document).ready(function(){
     top:   '2px'
   });
   
+  // TODO Rewrite this plugin... it sucks..
   // Set up infinite scrolling
   if($.fn.infinitescroll){
     $('#posts').infinitescroll({
@@ -139,9 +152,23 @@ $(document).ready(function(){
       nextSelector: '#next_page',
       itemSelector: '#posts .post, #posts hr',
       donetext: '[End of Posts]'
-    }, alterContentBlock);
+    }, window.alterContentBlock);
   }
+  
   // Set the entire colorbox modal to close on click, save for any buttons that may do something else
   $('#cboxWrapper, #cboxPhoto').css("cursor", "pointer").click( $.fn.colorbox.close );
+  
+  // Set up the #return_to_top button
+  $('#return_to_top').click(function(){
+    $.scrollTo(0, 1000);
+  });
+  
+  $(window).scroll(function(){
+    if( $(window).scrollTop() > $(window).height() ){
+      $('#return_to_top').show();
+    } else {
+      $('#return_to_top').hide();
+    }
+  });
 });
 
